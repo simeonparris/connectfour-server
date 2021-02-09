a85c5fd785b3cd22829fcfe954a1d0efe5ede8c0
simeonparris <simeon.parris@infinityworks.com>
2021-02-09 16:14:05 +0000

    add data validation check into addPlayer Function. Also add function at the end of index.js to initialise the game after the DOM has been created.

--- source/spec/index.spec.js
+++ source/spec/index.spec.js
@@ -1,10 +1,25 @@
 const index = require('../index');
 
 describe("Testing addPlayer function", () => {
-    test("accepts a valid player name and number", () => {
+    test("accepts an empty player name string and valid player number", () => {
+        const newPlayerName = '';
+        const newPlayerNumber = 2;
+        const functionOutput = index.addPlayer(newPlayerName, newPlayerNumber);
+        const expectedOutput = {name: "Little Miss No-Name"};
+        expect(functionOutput.toString()).toStrictEqual(expectedOutput.toString());
+    });
+    test("accepts a valid player name and valid number", () => {
         const newPlayerName = 'Fred';
         const newPlayerNumber = 1;
-        index.addPlayer(newPlayerName, newPlayerNumber);
-        expect(index.playerName).toStrictEqual('Fred');
+        const functionOutput = index.addPlayer(newPlayerName, newPlayerNumber);
+        const expectedOutput = {name: "Fred"};
+        expect(functionOutput.toString()).toStrictEqual(expectedOutput.toString());
+    });
+    test("does not accept an invalid player name and a valid number", () => {
+        const newPlayerName = 1;
+        const newPlayerNumber = 1;
+        const functionOutput = index.addPlayer(newPlayerName, newPlayerNumber);
+        const expectedOutput = undefined;
+        expect(functionOutput).toStrictEqual(expectedOutput);
     });
 });
