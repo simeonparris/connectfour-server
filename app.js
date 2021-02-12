096e9d3f180a594583f9bfc5aca01fb28680afb4
simeonparris <simeon.parris@infinityworks.com>
2021-02-12 16:56:33 +0000

    removed unneccessary require statements from the js files.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const classes = require('./classes');
-const pf = require('./pure-functions');
-
 console.log('Running app.js');
 // variables available in the global scope
 let playerOneObject = undefined;
@@ -12,8 +9,8 @@ let currentConnectFourGrid = [];
 function startGame () {
     const playerOneNameInput = document.getElementById('player-one-name-input').value;
     const playerTwoNameInput = document.getElementById('player-two-name-input').value;
-    playerOneObject = pf.addPlayer(playerOneNameInput, 1);
-    playerTwoObject = pf.addPlayer(playerTwoNameInput, 2);
+    playerOneObject = addPlayer(playerOneNameInput, 1);
+    playerTwoObject = addPlayer(playerTwoNameInput, 2);
     console.log(`Player One's name is ${playerOneObject.playerName} and Player Two's name is ${playerTwoObject.playerName}.`);
     currentConnectFourGrid = generateNewGrid(6, 7);
     hideNameInputBoxes();
@@ -24,14 +21,13 @@ function hideNameInputBoxes() {
     document.getElementById('player-two-name-display').append(playerTwoObject.playerName);
     document.getElementById('player-name-input-form').hidden = true;
     document.getElementById('start-button').hidden = true;
-    document.getElementById('player-name-display-area').hidden = false;
+    document.getElementById('player-name-display-area').display = "block";
     document.getElementById('box-to-fill').append('Here be TEXT!');
-
 }
 
 function generateNewGrid(numberOfRows, numberOfColumns) {
     let newGrid = new connectFourGrid(numberOfRows, numberOfColumns);
-    return newGrid.grid;
+    return newGrid.getGrid();
 }
 
 // Bindings for click events
--- pure-functions.js
+++ pure-functions.js
@@ -1,6 +1,3 @@
-const classes = require('./classes');
-
-
 function addPlayer(playerName, playerNumber) {
     // returns undefined if an invalid data type is entered
     if (typeof playerName !== 'string' || typeof playerNumber !== 'number') {
@@ -10,11 +7,11 @@ function addPlayer(playerName, playerNumber) {
     if (playerNumber === 1) {
         // assign player 1 name from HTML input or random name
         if (playerName === "") playerName = "Mr No-Name";
-        return new classes.Player(playerName, playerNumber);
+        return new Player(playerName, playerNumber);
     } else if (playerNumber === 2) {
         // assign player 2 name from HTML input or random name
         if (playerName === "") playerName = "Little Miss No-Name";
-        return new classes.Player(playerName, playerNumber);
+        return new Player(playerName, playerNumber);
     } else {
         console.log('addPlayer: something has gone seriously wrong!');
     }
